1f6840ccb0b2d097865804fc863c8547a85d7823
Sret Ksor <sretksorjiu.nguyen@gmail.com>
2021-05-03 08:21:46 +0700

    xong khoi qua trinh

--- 1.js
+++ 1.js
@@ -1,4 +1,17 @@
 ﻿ $(function(){
- 
+    //  ham tinh vi tri 
+    vitricuaedu = $('#khoiedu').offset().top;
+
+
+    // click chuot no di chuyen xuong
+    $(".xuong, .kn").click(function(){
+        $('body,html').animate({scrollTop:vitricuaedu},1000);
+        return false;
+    })
+    //click vao gioi thieu
+    $(".gt").click(function(){
+        $('body,html').animate({scrollTop:0},1000);
+        return false;
+    })
 })  
  
